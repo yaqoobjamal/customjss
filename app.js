0731449c8418a309e881212832bd30bd7e8611f0
yaqoobjamal <yaqoobjamal99@gmail.com>
2020-04-13 13:31:39 +0500

    Extra files removed

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 5000
 app.use(express.static(__dirname + "/"));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => console.log(`Example app listening at port ${port}!`))
 
