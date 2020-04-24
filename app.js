c3c6c5cd39f7d7847d431db108dddc6e3a23869c
usmance <usmankhalid38@outlook.com>
2020-04-24 12:57:35 +0500

    Issue in app.js solved

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const path = require('path')
 const app = express()
 const port = 5000;
 
-app.get('/temp',(req,res)=>{
-    res.send(myjson)
-})
+// app.get('/temp',(req,res)=>{
+//     res.send(myjson)
+// })
+app.use(express.static(__dirname + "/"));
+
+
+
+
 app.listen(port, () => console.log(`Example app listening at port ${port}!`))
 
 //
